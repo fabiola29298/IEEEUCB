da7a6e4b2d56db846ffb77bfcf750dcd16a81be7
Rodrigo Fernandez <sergiorodrigofernandeztesta@gmail.com>
2023-08-08 23:06:05 -0400

    :construction: [ADD] Directives
    TODO: Update pictures

---
+++ content/data.js
@@ -0,0 +1,365 @@
+const chapters_data = [
+    {
+        title: "Communications Society",
+        description: "La Communications Society promueve el avance de la ciencia, tecnología y aplicaciones en el área de las comunicaciones y disciplinas relacionadas. Fomenta la presentación y el intercambio de información entre sus miembros y las comunidades técnicas alrededor del mundo.",
+        subtitle: "Equipo de ComSoc",
+        image: "images/team2/comsoc.jpg",
+        webpage: "https://www.facebook.com/ComSoc.ucb/"
+    },
+    {
+        title: "Computer Society",
+        description: 'Su propósito y alcance es "avanzar en la teoría, la práctica y la aplicación de la ciencia y tecnología de procesamiento de la información y la computadora" y la"posición profesional de sus miembros". La CS es la más grande de las 39 sociedades técnicas del IEEE.',
+        subtitle: "Equipo de CS",
+        image: "images/team2/cs.jpg",
+        webpage: "https://www.facebook.com/IEEEComputerSocietyUCB/"
+    },
+    {
+        title: "Engineering in Medicine and Biology Society",
+        description: "Es la asociación profesional más grande del mundo dedicada a promover la innovación tecnológica y la excelencia para el beneficio de la humanidad. El IEEE y sus miembros inspiran a una comunidad global a través de las publicaciones, conferencias, estándares tecnológicos y actividades profesionales.",
+        subtitle: "Equipo de EMBS",
+        image: "images/team2/embs.jpg",
+        webpage: "http://bit.ly/Facebook_EMB_UCB"
+    },
+    {
+        title: "Nuclear and Plasma Sciences Society",
+        description: "El propósito es el encuentro de estudiantes y profesionales interesados en los campos de la energía nuclear y tecnologías de plasma, fomentar el intercambio de conocimientos y experiencias compartidas por expertos líderes en el campo, así como los avances más recientes. NPSS promueve el desarrollo profesional y el networking",
+        subtitle: "Equipo de NPSS",
+        image: "images/team2/npss.jpg",
+        webpage: "https://www.facebook.com/IEEE-NPSS-UCB-101501418496002/"
+    },
+    {
+        title: "Power and Energy Society",
+        description: "La IEEE Power & Energy Society (PES) es una sociedad técnica de IEEE dedicada exclusivamente al avance de la tecnología de energía eléctrica. La misión de PES es ser el recurso principal mundial para el desarrollo y la distribución de información, conexiones y recursos para profesionales de la energía eléctrica y miembros de la comunidad global de energía eléctrica.",
+        subtitle: "Equipo de PES",
+        image: "images/team2/pes.jpg",
+        webpage: "https://www.facebook.com/IEEE-PES-UCB-101501418496002/"
+    },
+    {
+        title: "Robotics and Automation Society",
+        description: "La IEEE Robotics and Automation Society es una sociedad profesional de IEEE que apoya el desarrollo y el intercambio de conocimiento científico en los campos de la robótica y la automatización, incluidas las cuestiones aplicadas y teóricas.",
+        subtitle: "Equipo de RAS",
+        image: "images/team2/ras.jpg",
+        webpage: "https://www.facebook.com/IEEE-RAS-UCB-529670894215502/"
+    },
+    {
+        title: "Signal Processing Society",
+        description: "La IEEE Signal Processing Society (SPS) es la primera sociedad de IEEE. Los miembros de SPS son tan diversos como el campo de procesamiento de señales. Su objetivo es mantener a los miembros al tanto de\nla información más reciente.",
+        subtitle: "Equipo de SPS",
+        image: "images/team2/sps.jpg",
+        webpage: "https://www.facebook.com/IEEE-SPS-UCB-2234736233443049/"
+    }
+];
+
+const team_data = [
+    {
+        name: "Msc. Fabio Richard Diaz Palacios",
+        position: "Mentor de la rama",
+        description: 'Director de la Carrera de Ingeniería Mecatrónica de la Univesidad Católica Boliviana "San Pablo"',
+        image: "images/directiva/FabioDiaz.jpg",
+        webpage: "https://www.linkedin.com/in/fabiodiazpalacios/"
+    },
+    {
+        name: "Sergio Rodrigo Fernandez Testa",
+        position: "Presidente",
+        description: "Estudiante de ingeniería mecatrónica , 10mo semestre",
+        image: "images/directiva/RodrigoFernandez.jpg",
+        webpage: "https://www.linkedin.com/in/sergio5919/"
+    },
+    {
+        name: "Ramón Walter Herrera Peralta",
+        position: "Vicepresidente",
+        description: "Estudiante de ingeniería mecatrónica , 9no semestre",
+        image: "images/directiva/RamonHerrera.jpeg",
+        webpage: "https://www.linkedin.com/in/ram%C3%B3n-herrera-peralta-876b06183/"
+    },
+    {
+        name: "Stephanie Belén Saavedra Ayarde",
+        position: "Secretaria General",
+        description: "Estudiante de ingeniería de sistemas, 9no semestre",
+        image: "images/directiva/StephanieSaavedra.jpg",
+        webpage: "https://www.linkedin.com/in/stephanie-saavedra-ayarde-9b0b3a1a0/"
+    },
+    {
+        name: "Maria Jose Rakela Drakic",
+        position: "Coordinadora de actividades estudiantiles",
+        description: "Egresada de ingeniería biomédica",
+        image: "images/directiva/MariaJoseRakela.jpeg",
+        webpage: "https://www.linkedin.com/in/maria-jose-rakela-0267431a8/"
+    },
+    {
+        name: "Juan Manuel Elías Soria",
+        position: "Coordinador nacional",
+        description: "Estudiante de ingeniería mecatrónica , 10mo semestre",
+        image: "images/directiva/ManuelElias.png",
+        webpage: "https://www.linkedin.com/in/juan-manuel-elias-soria/"
+    },
+    {
+        name: "Andrea Karen Ortíz Díaz",
+        position: "Coordinadora Internacional",
+        description: "Estudiante de ingeniería ambiental, 9no semestre",
+        image: "images/directiva/AndreaOrtiz.jpg",
+        webpage: "https://www.linkedin.com/in/andrea-ort%C3%ADz-d%C3%ADaz-45708921a/"
+    },
+    {
+        name: "Nadir Carla Cossio Bustillos",
+        position: "Coordinadora WIE",
+        description: "Estudiante de ingeniería biomédica, 10mo semestre",
+        image: "images/directiva/NadirCossio.jpg",
+        webpage: "https://www.linkedin.com/in/nadir-cossio-bustillos-0742651b3/"
+    },
+    {
+        name: "Hamed Emmerson Quenta Alvarez",
+        position: "Tesorero",
+        description: "Estudiante de ingeniería mecatrónica , 10mo semestre",
+        image: "images/directiva/HamedQuenta.png",
+        webpage: "https://www.linkedin.com/in/hamed-quenta/"
+    },
+    {
+        name: "Yamil Ignacio Paz Sea",
+        position: "Marketing",
+        description: "Estudiante de ingeniería de sistemas, 9no semestre",
+        image: "images/directiva/YamilPaz.jpg",
+        webpage: "https://www.linkedin.com/in/ignacio-paz-sea/"
+    },
+    {
+        name: "Fernando Miguel Calderón Villanueva",
+        position: "Vocal",
+        description: "Estudiante de ingeniería mecatrónica , 8vo semestre",
+        image: "images/directiva/FernandoCalderon.jpeg",
+        webpage: ""
+    }
+];
+
+
+
+const full_team_data = [
+    {
+        name: "Sofia Alejandra Arce Chavez",
+        position: "Presidente de ComSoc",
+        description: "Estudiante de ingeniería de Telecomunicaciones",
+        image: "images/capitulos/ComSoc/SofiaAlejandraArceChavez.jpg"
+    },
+    {
+        name: "Victor Eduardo Nina Arancibia",
+        position: "Vicepresidente de ComSoc",
+        description: "Estudiante de ingeniería de Telecomunicaciones",
+        image: "images/capitulos/ComSoc/VictorEduardoNinaArancibia.jpg"
+    },
+    {
+        name: "Alejandra Aban",
+        position: "Secretaria general de ComSoc",
+        description: "Estudiante de ingeniería de Telecomunicaciones",
+        image: "images/capi/AlejandraAban.jpgtulos/ComSoc"
+    },
+    {
+        name: "Roger Espejo Gutierrez",
+        position: "Encargado de  comunicaciones de ComSoc",
+        description: "Estudiante de ingeniería de Telecomunicaciones",
+        image: "images/capitulos/Co/RogerEspejoGutierrez.jpgmSoc"
+    },
+    {
+        name: "Radomir Luis Vladislavic Nolasco",
+        position: "Presidente de CS",
+        description: "Estudiante de ingeniería de Sistemas",
+        image: "images/capitulos/CS/RadomirLuisVladislavicNolasco.jpg"
+    },
+    {
+        name: "Ignacio Andres Illanes Bequer",
+        position: "Vicepresidente de CS",
+        description: "Estudiante de ingeniería de Sistemas",
+        image: "images/capitulos/CS/IgnacioAndresIllanesBequer.jpg"
+    },
+    {
+        name: "Oscar Campohermoso Berdeja",
+        position: "Secretario general de CS",
+        description: "Estudiante de ingeniería de Sistemas",
+        image: "images/capitulos/CS/OscarCampohermosoBerdeja.jpg"
+    },
+    {
+        name: "Oscar Alfredo Menacho Silva",
+        position: "Encargado de  comunicaciones de CS",
+        description: "Estudiante de ingeniería de Sistemas",
+        image: "images/capitulos/CS/OscarAlfredoMenachoSilva.jpg"
+    },
+    {
+        name: "Alejandra Inarra Ruiz",
+        position: "Presidente de EMBS",
+        description: "Estudiante de ingeniería Biomédica",
+        image: "images/capitulos/E/AlejandraInarraRuiz.jpgMBS"
+    },
+    {
+        name: "Nataly Alcazar",
+        position: "Vicepresidente de EMBS",
+        description: "Estudiante de ingeniería Biomédica",
+        image: "images/capi/NatalyAlcazar.jpgtulos/EMBS"
+    },
+    {
+        name: "Fernanda Saleg",
+        position: "Secretaria general de EMBS",
+        description: "Estudiante de ingeniería Biomédica",
+        image: "images/capi/FernandaSaleg.jpgtulos/EMBS"
+    },
+    {
+        name: "Génesis Paola Maydana",
+        position: "Encargada de  comunicaciones de EMBS",
+        description: "Estudiante de ingeniería Biomédica",
+        image: "images/capitulos/E/GénesisPaolaMaydana.jpgMBS"
+    },
+    {
+        name: "Diana Medrano Sauma",
+        position: "Presidente de NPSS",
+        description: "Estudiante de ingeniería de Química",
+        image: "images/capitulos/DianaMedranoSauma.jpg/NPSS"
+    },
+    {
+        name: "Nicolas Esteban Donoso Torres",
+        position: "Vicepresidente de NPSS",
+        description: "Estudiante de ingeniería de Química",
+        image: "images/capitulos/NPSS/NicolasEstebanDonosoTorres.jpg"
+    },
+    {
+        name: "Paulo Adrian Fernandez Leguia",
+        position: "Secretario General de NPSS",
+        description: "Estudiante de ingeniería de Química",
+        image: "images/capitulos/NPSS/PauloAdrianFernandezLeguia.jpg"
+    },
+    {
+        name: "Gilda Mariana Torrico Miranda",
+        position: "Encargada de comunicaciones de NPSS",
+        description: "Estudiante de ingeniería de Química",
+        image: "images/capitulos/NPSS/GildaMarianaTorricoMiranda.jpg"
+    },
+    {
+        name: "Ever Imanol Alavi Gallardo",
+        position: "Presidente de PES",
+        description: "Estudiante de ingeniería Mecatrónica",
+        image: "images/capitulos/PES/EverImanolAlaviGallardo.jpg"
+    },
+    {
+        name: "Amancaya Jeezrel Mattaz Huampu",
+        position: "Vicepresidente de PES",
+        description: "Estudiante de ingeniería Mecatrónica",
+        image: "images/capitulos/PES/AmancayaJeezrelMattazHuampu.jpg"
+    },
+    {
+        name: "Mauren Mendoza Hoyos",
+        position: "Secretaria general de PES",
+        description: "Estudiante de ingeniería de Química",
+        image: "images/capitulos//MaurenMendozaHoyos.jpgPES"
+    },
+    {
+        name: "Melany Gretzel Marca Peña",
+        position: "Encargada de comunicaciones de PES",
+        description: "Estudiante de ingeniería de Ambiental",
+        image: "images/capitulos/PES/MelanyGretzelMarcaPeña.jpg"
+    },
+    {
+        name: "Jaime Alejandro Condori Machaca",
+        position: "Presidente de RAS",
+        description: "Estudiante de ingeniería Mecatrónica",
+        image: "images/capitulos/RAS/JaimeAlejandroCondoriMachaca.jpg"
+    },
+    {
+        name: "Sinei Libny Ortega Ortega",
+        position: "Vicepresidente de RAS",
+        description: "Estudiante de ingeniería Mecatrónica",
+        image: "images/capitulos/RAS/SineiLibnyOrtegaOrtega.jpg"
+    },
+    {
+        name: "Alejandro Matías Mora López",
+        position: "Secretario general de RAS",
+        description: "Estudiante de ingeniería Mecatrónica",
+        image: "images/capitulos/RAS/AlejandroMatíasMoraLópez.jpg"
+    },
+    {
+        name: "Leoni Marti Miranda Saravia",
+        position: "Encargado de comunicaciones de RAS",
+        description: "Estudiante de ingeniería Mecatrónica",
+        image: "images/capitulos/RAS/LeoniMartiMirandaSaravia.jpg"
+    },
+    {
+        name: "Álvaro Mauricio González Ramírez",
+        position: "Presidente de SPS",
+        description: "Estudiante de ingeniería de Telecomunicaciones",
+        image: "images/capitulos/SPS/ÁlvaroMauricioGonzálezRamírez.jpg"
+    },
+    {
+        name: "Damaris Soledad Paredes Plata",
+        position: "Vicepresidente de SPS",
+        description: "Estudiante de ingeniería de Telecomunicaciones",
+        image: "images/capitulos/SPS/DamarisSoledadParedesPlata.jpg"
+    },
+    {
+        name: "Nagely Nicole Rodriguez Torrez",
+        position: "Secretaria general de SPS",
+        description: "Estudiante de ingeniería Biomédica",
+        image: "images/capitulos/SPS/NagelyNicoleRodriguezTorrez.jpg"
+    },
+    {
+        name: "Alexander José Mauricio Quisbert Callejas",
+        position: "Encargado de comunicaciones de SPS",
+        description: "Estudiante de ingeniería de Telecomunicaciones",
+        image: "images/capitulos/SPS/AlexanderJoséMauricioQuisbertCallejas.jpg"
+    }
+]
+
+// Ing. Mauricio Horacio Aliaga Villacorta	Mentor	ComSoc	Telecom	mh.aliaga@ieee.org	72568081	72568081
+// Ing. Vicente Yamil Cárdenas Miguel	Mentor	CS	Sistemas	vycardenas@ieee.org	72093333	98290890
+// Ing. Susana Gloria Vargas Mendieta	Mentor	EMBS	Biomédica	susana.vargas@ieee.org	70236911	92313711
+// Ing. Valeria Fabiana Calani San Miguel 	Mentor	NPSS	Química	valeria.calani@ieee.org	77554765	
+// Ing. Afnan Agramont Akiyama	Mentor	PES	Ambiental		67011565	
+// Ing. Marcelo Saavedra Alcoba	Mentor	RAS	Mecatrónica	msaavedra@ieee.org	71971226	95494712
+// Ph.D. Edgar Eduardo Salazar Florez	Mentor	SPS	Mecatrónica			
+
+
+const mentor_team_data = [
+    {
+        name: "Ing. Mauricio Horacio Aliaga Villacorta",
+        position: "Mentor de ComSoc",
+        description: "Docente de la carrera de ingeniería de Telecomunicaciones",
+        image: "images/capitulos/ComSoc/MauricioHoracioAliagaVillacorta.jpg",
+        webpage: ""
+    },
+    {
+        name: "Ing. Vicente Yamil Cárdenas Miguel",
+        position: "Mentor de CS",
+        description: "Docente de la carrera de ingeniería de Sistemas",
+        image: "images/capitulos/CS/VicenteYamilCárdenasMiguel.jpg",
+        webpage: ""
+    },
+    {
+        name: "Ing. Susana Gloria Vargas Mendieta",
+        position: "Mentor de EMBS",
+        description: "Docente de la carrera de ingeniería Biomédica",
+        image: "images/capitulos/EMBS/SusanaGloriaVargasMendieta.jpg",
+        webpage: ""
+    },
+    {
+        name: "Ing. Valeria Fabiana Calani San Miguel",
+        position: "Mentor de NPSS",
+        description: "Docente de la carrera de ingeniería de Química",
+        image: "images/capitulos/NPSS/ValeriaFabianaCalaniSanMiguel.jpg",
+        webpage: ""
+    },
+    {
+        name: "Ing. Afnan Agramont Akiyama",
+        position: "Mentor de PES",
+        description: "Docente de la carrera de ingeniería de Ambiental",
+        image: "images/capitulos/PES/AfnanAgramontAkiyama.jpg",
+        webpage: ""
+    },
+    {
+        name: "Ing. Marcelo Saavedra Alcoba",
+        position: "Mentor de RAS",
+        description: "Docente de la carrera de ingeniería Mecatrónica",
+        image: "images/capitulos/RAS/MarceloSaavedraAlcoba.jpg",
+        webpage: ""
+    },
+    {
+        name: "Ph.D. Edgar Eduardo Salazar Florez",
+        position: "Mentor de SPS",
+        description: "Docente de la carrera de ingeniería Mecatrónica",
+        image: "images/capitulos/SPS/EdgarEduardoSalazarFlorez.jpg",
+        webpage: ""
+    }
+];
